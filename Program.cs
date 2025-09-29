4d9a50ea8eed08e8cf9cadc7ac74ba31fb03b9bd
lumencalm <M.Haas@htlvb.at>
2025-09-29 09:15:34 +0200

    only stolen code v2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Simulanten - Übung");
-Console.WriteLine("Rah Rah Rasputin (from alice in borderlands");
+Console.WriteLine("Rah Rah Rasputin (from alice in borderlands)");
+Console.WriteLine("Rah Rah Rasputin (from alice in borderlands v2)");
